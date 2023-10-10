6d93900ff6cdae180f7b2b39b4f676e7a831525d
Gustas11 <vilimas.gustas@gmail.com>
2023-10-10 21:34:09 +0100

    simple fix

--- getwrapofdaykfc/main.py
+++ getwrapofdaykfc/main.py
@@ -4,16 +4,7 @@ def main():
     time = datetime.now()
     day = time.weekday()
     print(day)
-
-    if(day == 1 or day == 5):
-        print("Wrap of the day is Kentucky Mayo")
-    elif(day == 2):
-        print("Wrap of the day is Spicy SuperCharger Mayo")
-    elif(day == 3):
-        print("Wrap of the day is Somkey BBQ")
-    elif(day == 4):
-        print("Wrap of the day is Sweet Chili")
-    else:
-        print("There is none for today :(")
-
+    wrap = ["Kentucky Mayo", "Spicy SuperCharger Mayo", "Somkey BBQ", "Sweet Chili", "Kentucky Mayo", "None", "None"]
+    print("The wrap of the day is " + wrap[day])
+   
 main()
